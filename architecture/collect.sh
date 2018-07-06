592b4424a2481437d9f23316513704079175c1bc
Truong Thao Nguyen <nguyen.88.tt@gmail.com>
2018-07-06 19:05:40 +0900

    result for Dragonfly

---
+++ architecture/collect.sh
@@ -0,0 +1,5 @@
+grep "33554432" parse.txt > collect.txt
+echo "----------------" >> collect.txt
+
+grep "33554432" energy_parse.txt > energy_collect.txt
+echo "----------------" >> energy_collect.txt
